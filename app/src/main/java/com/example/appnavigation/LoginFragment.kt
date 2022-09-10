014996f317ebccf672ef6db3c5cfc29a87956728
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2022-09-10 15:57:16 +0300

    AppNavigation - Login and Main graph created

--- app/src/main/java/com/example/appnavigation/LoginFragment.kt
+++ app/src/main/java/com/example/appnavigation/LoginFragment.kt
@@ -5,13 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.appnavigation.databinding.FragmentLoginBinding
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 
 class LoginFragment : Fragment() {
@@ -23,7 +18,6 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        //fragmentLoginBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
         fragmentLoginBinding = FragmentLoginBinding.inflate(layoutInflater)
         return fragmentLoginBinding.root
     }
@@ -31,11 +25,9 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fragmentLoginBinding.apply {
-            /* button.setOnClickListener {
-                 findNavController().navigate(R.id.action_loginFragment_to_forgetPasswordFragment)*/
+            buttonLogin.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+            }
         }
-
     }
 }
-
-
